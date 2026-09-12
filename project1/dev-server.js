a049130573d766c1fe5e5e252ded6e2e266cd138
footballjay <nhj1196@gmail.com>
2026-09-12 11:05:30 +0900

    Delete project1 directory

--- project1/dev-server.js
+++
@@ -1,3 +0,0 @@
-const http=require('http');const fs=require('fs');const path=require('path');
-const root=__dirname;const types={'.html':'text/html; charset=utf-8','.js':'text/javascript; charset=utf-8','.css':'text/css; charset=utf-8','.png':'image/png'};
-http.createServer((req,res)=>{const requestPath=decodeURIComponent(req.url.split('?')[0]);const filePath=path.join(root,requestPath==='/'?'index.html':requestPath);if(!filePath.startsWith(root)){res.writeHead(403);return res.end('Forbidden')}fs.readFile(filePath,(err,data)=>{if(err){res.writeHead(404);return res.end('Not found')}res.writeHead(200,{'Content-Type':types[path.extname(filePath)]||'application/octet-stream'});res.end(data)})}).listen(4173,()=>console.log('나만의 A.T local server: http://localhost:4173'));
